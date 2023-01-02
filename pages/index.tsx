14eecf6ce3a7bfd47f652cf2f24aeff963dde4c9
Yuri Failer <yuri.failer@gmail.com>
2023-01-02 10:22:16 +0200

    wip.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,22 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-
+import bg from '../public/141210__36A1655.jpg'
+import {useState} from "react";
+type Language = 'en' | 'he' | 'ru';
 
 export default function Home() {
+  const [lang, setLang] = useState<Language>('en');
+  const content: { [key in Language]?: {
+    name: string,
+    list: string[]
+  } } = {
+    en: {
+      name: 'Lidia Nekliuienko',
+      list: ['Drawing teacher for all ages', 'Pictures, portraits to order'],
+    },
+  };
+  const { name, list } = content[lang]!;
   return (
     <>
       <Head>
@@ -12,11 +25,12 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="w-[90vw] h-[90vh] rounded ">
-        <h1>Lidia Nekliuienko</h1>
+      <main className="w-[90vw] h-[90vh] rounded bg-cover" style={{
+        backgroundImage: `url(${bg.src})`,
+      }}>
+        <h1>{name}</h1>
         <ul>
-          <li>Drawing teacher for all ages</li>
-          <li>Pictures, portraits to order</li>
+          { list.map((item) => <li key={item.slice(0, 9)}>{item}</li>) }
         </ul>
         <div>
           <a href="https://facebook.com/lipane.art">facebook.com/lipane.art</a>
