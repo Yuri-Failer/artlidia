5cad6d38a508e3fcf85fa4f41b2f8be120acfe1c
Yuri Failer <yuri.failer@gmail.com>
2023-10-03 12:07:25 +0300

    Start wall frame template.

---
+++ pages/artwork/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Frame = () => {
+  return (
+    <div className='w-full h-[100vh] flex'>
+      <div className='border-8 border-amber-600 w-5/6 m-auto mt-auto bg-amber-50'>
+        <img className='p-6' src={'/elefants.jpg'} />
+      </div>
+    </div>
+  );
+};
+
+export default Frame;
