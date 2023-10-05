dcd8a2b660b2fde1accef8fa7ce7785c05992f5c
Yuri Failer <yuri.failer@gmail.com>
2023-10-05 14:55:33 +0300

    WIP. add classic frame.

--- pages/artwork/index.tsx
+++ pages/artwork/index.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import style from 'pages/artwork/artwork.module.css';
 
 const Frame = () => {
   return (
-    <div className='w-full h-[100vh] flex'>
-      <div className='border-8 border-amber-600 w-5/6 m-auto mt-auto bg-amber-50'>
-        <img className='p-6' src={'/elefants.jpg'} />
+    <div>
+      {/*<div className='border-8 border-amber-600 w-4/6 m-auto bg-amber-50'>*/}
+      {/*<div className="bg-yellow-300 border border-solid border-5vmin border-eee border-t-ddd border-r-eee border-b-fff border-l-eee rounded-2 box-shadow-inner-md box-shadow-md m-10vh 10vw h-80vh p-8 relative text-center">*/}
+       <div className={style.frameClassic}>
+        <img className={style.imgClassic} src={'/elefants.jpg'} />
       </div>
+
+      {/*<div className='bg-amber-100'>*/}
+      {/*  <div>Elefants, 2020 </div>*/}
+      {/*  <div>oil on canvas </div>*/}
+      {/*  <div>50x70 cm</div>*/}
+      {/*</div>*/}
     </div>
   );
 };
