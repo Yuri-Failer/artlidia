fa5b7bca05bf036f03bb8acf8cd4c4ef5b263cb7
Yuri Failer <yuri.failer@gmail.com>
2023-10-14 01:24:03 +0300

    update packages, mdx support, eslint, airbnb.

--- pages/artwork/index.tsx
+++ pages/artwork/index.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import style from 'pages/artwork/artwork.module.css';
 
-const Frame = () => {
+function Frame() {
   return (
-    <div className="flex flex-col">
-       <div className={style.frameClassic}>
-        <img className={style.imgClassic} src={'/elefants.jpg'} />
+    <div className={`${style.bgWall} flex flex-col`}>
+      <div className={style.blur}>
+        <div className={style.frameClassic}>
+          <img className={style.imgClassic} src="/elefants.jpg" />
+        </div>
       </div>
 
-      <div className='bg-amber-100'>
+      <div className="bg-amber-100">
         <div>Elefants, 2020 </div>
         <div>oil on canvas </div>
         <div>50x70 cm</div>
       </div>
     </div>
   );
-};
+}
 
 export default Frame;
