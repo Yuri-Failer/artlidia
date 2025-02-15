29a0af55c0cf1a8ebce428b4fbfbce6e397dff75
Yuri Failer <yuri.failer@gmail.com>
2025-02-15 15:34:44 +0200

    update packages, mdx support, eslint, airbnb.

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const withMDX = require('@next/mdx')()
-const nextConfig = {
-  reactStrictMode: true,
-  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
-}
-
-module.exports = withMDX(nextConfig)
--- pages/artwork/index.tsx
+++ pages/artwork/index.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
-import style from 'pages/artwork/artwork.module.css';
+import style from './artwork.module.css';
 
-function Frame() {
+function Frame({title}: {title: string}) {
   return (
     <div className={`${style.bgWall} flex flex-col`}>
       <div className={style.blur}>
-        <div className={style.frameClassic}>
-          <img className={style.imgClassic} src="/elefants.jpg" />
+        <div className="flex flex-col items-center p-10 sm:p-6">
+          <div className={style.frameClassic}>
+            <img className={style.imgClassic} src="/elefants.jpg" />
+          </div>
+          <div className="bg-amber-100 flex flex-col items-center justify-center mt-auto w-[40%] p-2">
+            <div>{title}, 2020</div>
+            <div>oil on canvas</div>
+            <div>50x70 cm</div>
+          </div>
         </div>
-      </div>
-
-      <div className="bg-amber-100">
-        <div>Elefants, 2020 </div>
-        <div>oil on canvas </div>
-        <div>50x70 cm</div>
       </div>
     </div>
   );
